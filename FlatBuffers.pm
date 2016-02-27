e7320a66841613541dcd39bc071961817ebb4af9
mirror <mirror12k@gmail.com>
2016-02-27 16:48:51 -0500

    implemented grammar from the flatbuffers page

--- FlatBuffers.pm
+++ FlatBuffers.pm
@@ -9,3 +9,46 @@ use feature 'say';
 # little-endian everything
 
 
+
+# these weren't defined in the grammar. gj google
+my $regex_string_constant = qr/"[^"]*"/xs;
+my $regex_ident = qr/[a-zA-Z_][a-zA-Z_\d]*/x;
+
+# definition taken from https://google.github.io/flatbuffers/flatbuffers_grammar.html
+
+my $regex_integer_constant = qr/ -?\d+ | true | false /x;
+my $regex_float_constant = qr/ -?\d+\.\d+([eE][+\-]?\d+)? /x; 
+my $regex_type = qr/ (?<regex_type> bool | byte | ubyte | short | ushort | int | uint | float | long | ulong | double | string | \[\s*(?&regex_type)\s*\] | $regex_ident ) /x;
+
+my $regex_scalar = qr/ $regex_integer_constant | $regex_float_constant/x;
+my $regex_single_value = qr/ $regex_scalar | $regex_string_constant /x;
+my $regex_metadata = qr/ (\( $regex_ident (\s*:\s*$regex_single_value )? (\s*,\s*$regex_ident (\s*:\s*$regex_single_value )? )* \) )? /x;
+
+my $regex_file_extension_decl = qr/ file_extension\s+$regex_string_constant\s*; /x;
+my $regex_file_identifier_decl = qr/ file_identifier\s+$regex_string_constant\s*; /x;
+my $regex_include = qr/ include\s+$regex_string_constant\s*; /x;
+my $regex_namespace_decl = qr/ namespace\s+$regex_ident ( \. $regex_ident )*\s*; /x;
+my $regex_root_decl = qr/ root_type\s+$regex_ident\s*; /x;
+my $regex_attribute_decl = qr/ attribute\s+$regex_string_constant\s*; /x;
+
+my $regex_enumval_decl = qr/ $regex_ident (\s*=\s*$regex_integer_constant)? /x;
+my $regex_enum_decl = qr/ ( enum | union )\s+$regex_ident\s*(:\s*$regex_type\s+)? $regex_metadata \{ ($regex_enumval_decl (\s*,\s*$regex_enumval_decl)*)? \} /x;
+
+my $regex_field_decl = qr/ $regex_ident\s*:\s*$regex_type\s*(=\s*$regex_scalar\s+)? $regex_metadata\s*;/x;
+my $regex_type_decl = qr/ ( table | struct )\s+$regex_ident\s+$regex_metadata\s+\{\s*($regex_field_decl)+\s*\} /x;
+
+
+# schema = include* ( namespace_decl | type_decl | enum_decl | root_decl | file_extension_decl | file_identifier_decl | attribute_decl | object )*
+
+# i have no clue what object is, the {} aren't strings in the schema, and i can't seem to find an explanation on what they are in schema
+# object = { commasep( ident : value ) }
+# value = single_value | object | [ commasep( value ) ]
+
+# commasep(x) = [ x ( , x )* ] # nope # this can actually be done using a function, but why would i?
+
+
+
+
+
+
+
