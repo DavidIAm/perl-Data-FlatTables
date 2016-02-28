215557bc776a4067db0204cfa43df4521f27a5ba
mirror <mirror12k@gmail.com>
2016-02-27 19:25:24 -0500

    - added basic syntax parsing via the scheme regex and a few test files

--- FlatBuffers.pm
+++ FlatBuffers.pm
@@ -9,6 +9,11 @@ use feature 'say';
 # little-endian everything
 
 
+sub strip_string (;$) {
+	(shift // $_) =~ s/\A"(.*)"\Z/$1/sr
+}
+
+
 
 # these weren't defined in the grammar. gj google
 my $regex_string_constant = qr/"[^"]*"/xs;
@@ -18,29 +23,30 @@ my $regex_ident = qr/[a-zA-Z_][a-zA-Z_\d]*/x;
 
 my $regex_integer_constant = qr/ -?\d+ | true | false /x;
 my $regex_float_constant = qr/ -?\d+\.\d+([eE][+\-]?\d+)? /x; 
-my $regex_type = qr/ (?<regex_type> bool | byte | ubyte | short | ushort | int | uint | float | long | ulong | double | string | \[\s*(?&regex_type)\s*\] | $regex_ident ) /x;
+my $regex_type = qr/ (?<regex_type_recurse> 
+	bool | byte | ubyte | short | ushort | int | uint | float | long | ulong | double | string | \[\s*(?&regex_type_recurse)\s*\] | $regex_ident ) /x;
 
 my $regex_scalar = qr/ $regex_integer_constant | $regex_float_constant/x;
 my $regex_single_value = qr/ $regex_scalar | $regex_string_constant /x;
 my $regex_metadata = qr/ (\( $regex_ident (\s*:\s*$regex_single_value )? (\s*,\s*$regex_ident (\s*:\s*$regex_single_value )? )* \) )? /x;
 
-my $regex_file_extension_decl = qr/ file_extension\s+$regex_string_constant\s*; /x;
-my $regex_file_identifier_decl = qr/ file_identifier\s+$regex_string_constant\s*; /x;
-my $regex_include = qr/ include\s+$regex_string_constant\s*; /x;
-my $regex_namespace_decl = qr/ namespace\s+$regex_ident ( \. $regex_ident )*\s*; /x;
-my $regex_root_decl = qr/ root_type\s+$regex_ident\s*; /x;
-my $regex_attribute_decl = qr/ attribute\s+$regex_string_constant\s*; /x;
+my $regex_file_extension_decl = qr/ file_extension\s+(?<file_extension_name> $regex_string_constant)\s*; /x;
+my $regex_file_identifier_decl = qr/ file_identifier\s+(?<file_identifier_name> $regex_string_constant)\s*; /x;
+my $regex_include = qr/ include\s+(?<include_name> $regex_string_constant)\s*; /x;
+my $regex_namespace_decl = qr/ namespace\s+(?<namespace_name> $regex_ident ( \. $regex_ident )*)\s*; /x;
+my $regex_root_decl = qr/ root_type\s+(?<root_name> $regex_ident)\s*; /x;
+my $regex_attribute_decl = qr/ attribute\s+(?<attribute_name> $regex_string_constant)\s*; /x;
 
 my $regex_enumval_decl = qr/ $regex_ident (\s*=\s*$regex_integer_constant)? /x;
 my $regex_enum_decl = qr/ ( enum | union )\s+$regex_ident\s*(:\s*$regex_type\s+)? $regex_metadata \{ ($regex_enumval_decl (\s*,\s*$regex_enumval_decl)*)? \} /x;
 
 my $regex_field_decl = qr/ $regex_ident\s*:\s*$regex_type\s*(=\s*$regex_scalar\s+)? $regex_metadata\s*;/x;
-my $regex_type_decl = qr/ ( table | struct )\s+$regex_ident\s+$regex_metadata\s+\{\s*($regex_field_decl)+\s*\} /x;
+my $regex_type_decl = qr/ ( table | struct )\s+$regex_ident\s+$regex_metadata\s*\{\s*($regex_field_decl\s*)+\s*\} /x;
 
 
 # schema = include* ( namespace_decl | type_decl | enum_decl | root_decl | file_extension_decl | file_identifier_decl | attribute_decl | object )*
 
-# i have no clue what object is, the {} aren't strings in the schema, and i can't seem to find an explanation on what they are in schema
+# i have no clue what object is, the {} aren't strings in the schema, and i can't seem to find an explanation on what they are in scheme
 # object = { commasep( ident : value ) }
 # value = single_value | object | [ commasep( value ) ]
 
@@ -48,7 +54,94 @@ my $regex_type_decl = qr/ ( table | struct )\s+$regex_ident\s+$regex_metadata\s+
 
 
 
+sub new {
+	my $class = shift;
+	my %args = @_;
+	my $self = bless {}, $class;
+
+	return $self
+}
 
 
 
+
+sub parse {
+	my ($self, $text) = @_;
+
+	my $lastpos;
+	while ($text =~ /\G \s*(
+		$regex_file_extension_decl |
+		$regex_file_identifier_decl |
+		$regex_include |
+		$regex_namespace_decl |
+		$regex_root_decl |
+		$regex_attribute_decl |
+
+		(?<enum_declaration>$regex_enum_decl) |
+		(?<type_declaration>$regex_type_decl)
+	) \s* /xg) {
+		my (
+			$file_extension_name,
+			$file_identifier_name,
+			$include_name,
+			$namespace_name,
+			$root_name,
+			$attribute_name,
+			$enum_declaration,
+			$type_declaration,
+		) = @+{qw/
+			file_extension_name
+			file_identifier_name
+			include_name
+			namespace_name
+			root_name
+			attribute_name
+			enum_declaration
+			type_declaration
+		/};
+		$lastpos = pos $text;
+		if (defined $file_extension_name) {
+			$file_extension_name = strip_string $file_extension_name;
+			say "got file extension: $file_extension_name";
+		} elsif (defined $file_identifier_name) {
+			$file_identifier_name = strip_string $file_identifier_name;
+			say "got file identifier: $file_identifier_name";
+		} elsif (defined $include_name) {
+			$include_name = strip_string $include_name;
+			say "got include: $include_name";
+		} elsif (defined $namespace_name) {
+			say "got namespace: $namespace_name";
+		} elsif (defined $root_name) {
+			say "got root: $root_name";
+		} elsif (defined $attribute_name) {
+			$attribute_name = strip_string $attribute_name;
+			say "got attribute: $attribute_name";
+		} elsif (defined $enum_declaration) {
+			say "got enum_declaration: $enum_declaration";
+		} elsif (defined $type_declaration) {
+			say "got type_declaration: $type_declaration";
+		} else {
+			...
+		}
+	}
+
+	if ($lastpos != length $text) {
+		warn "failed to parse file!!!!!!!!!!!!!!!!!!!!!!";
+		warn "error at file position $lastpos:\n";
+		warn substr $text, $lastpos, 100;
+	}
+}
+
+
+
+use File::Slurp qw/ read_file /;
+sub main {
+	my ($filepath) = @_;
+	my $text = read_file($filepath);
+
+	my $parser = FlatBuffers->new;
+	$parser->parse($text);
+}
+
+caller or main(@ARGV)
 
